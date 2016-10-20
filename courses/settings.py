f7dd2bf480c619f74b12624f9734dfe74f3de700
conniechu929 <conniechu929@gmail.com>
2016-10-20 14:21:15 -0700

    made changes to settings and added migrations

--- courses/settings.py
+++ courses/settings.py
@@ -77,18 +77,8 @@ WSGI_APPLICATION = 'courses.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'friends',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': '/Applications/MAMP/tmp/mysql/mysql.sock',
-        'PORT': '8889',
     }
 }
 
